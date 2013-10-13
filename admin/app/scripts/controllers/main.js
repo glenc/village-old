06351bad2573072c37c818cef7dfe578a3f98cce
Glen Cooper <glencooper@gmail.com>
2013-10-13 14:27:24 -0700

    Started adding admin site

---
+++ admin/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('villageAdminApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
